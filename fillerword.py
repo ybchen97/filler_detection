98647776afc89250fc58ed84cba735b0183fb071
Larry Law <larrylaw97@gmail.com>
2020-03-10 21:13:15 +0800

    Add 1.3. TODO: Fix spectrogram.

--- fillerword.py
+++ fillerword.py
@@ -21,13 +21,13 @@ NEGATIVES_TRUNCATED_DIRECTORY = "./raw_data/google_dataset_truncated/"
 # Load raw audio files for speech synthesis
 def load_raw_audio(positiveDirectory, backgroundDirectory, negativesDirectory):
     BACKGROUND_DIRECTORY_IN_GOOGLE_DATASET = "_background_noise_/"
-    activates = []
+    positives = []
     backgrounds = []
     negatives = []
     for filename in os.listdir(positiveDirectory):
         if filename.endswith("wav"):
             activate = AudioSegment.from_wav(positiveDirectory + filename)
-            activates.append(activate)
+            positives.append(activate)
     for filename in os.listdir(backgroundDirectory):
         if filename.endswith("wav"):
             background = AudioSegment.from_wav(backgroundDirectory + filename)
@@ -38,12 +38,12 @@ def load_raw_audio(positiveDirectory, backgroundDirectory, negativesDirectory):
                 if filename.endswith("wav"):
                     negative = AudioSegment.from_wav(negativesDirectory + directory + "/" + filename)
                     negatives.append(negative)
-    return activates, negatives, backgrounds
+    return positives, negatives, backgrounds
 
 # Load audio segments using pydub 
-# activates, negatives, backgrounds = load_raw_audio(POSITIVE_DIRECTORY, BACKGROUND_DIRECTORY, NEGATIVES_DIRECTORY)
-activates, negatives, backgrounds = load_raw_audio(POSITIVE_DIRECTORY, BACKGROUND_DIRECTORY, NEGATIVES_TRUNCATED_DIRECTORY)
+# positives, negatives, backgrounds = load_raw_audio(POSITIVE_DIRECTORY, BACKGROUND_DIRECTORY, NEGATIVES_DIRECTORY)
+positives, negatives, backgrounds = load_raw_audio(POSITIVE_DIRECTORY, BACKGROUND_DIRECTORY, NEGATIVES_TRUNCATED_DIRECTORY)
 
 print("background len: " + str(len(backgrounds[0])))    # Should be 10,000, since it is a 10 sec clip
-print("activate[0] len: " + str(len(activates[0])))     # Maybe around 1000, since an "activate" audio clip is usually around 1 sec (but varies a lot)
-print("activate[1] len: " + str(len(activates[1])))     # Different "activate" clips can have different lengths
+print("activate[0] len: " + str(len(positives[0])))     # Maybe around 1000, since an "activate" audio clip is usually around 1 sec (but varies a lot)
+print("activate[1] len: " + str(len(positives[1])))     # Different "activate" clips can have different lengths
